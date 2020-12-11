72b5935775253059632a56d85e71b83df0f53343
AdamOleksy <adamoleksy1983@gmail.com>
2020-12-11 12:57:03 +0100

    First commit

---
+++ Source/MyProject/MyProjectCharacter.h
@@ -0,0 +1,121 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyProjectCharacter.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemClass : uint8
+{
+	NONE,
+	SPEED_BOOSTER
+};
+
+UCLASS(config=Game)
+class AMyProjectCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
+	/* Use to boost set in editor */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement Parameters", meta = (AllowPrivateAccess = "true"))
+	float SpeedMultipier = 1.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	float PlayerBaseHP = 100.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	float PlayerCurrentHP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	float BoostDuration = 5.f;
+
+	/* Boost time left */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Player", meta = (AllowPrivateAccess = "true"))
+	float BoostCountDown;
+
+	float BoostCountDownInterval = 0.1f;
+
+	/* Used to handle counting down boost */
+	FTimerHandle BoostTimeHandle;
+
+	/*Reset player speed */
+	void ResetBoost();
+
+	/*To check if boost time have end*/
+	void CheckBoostTimeLeft();
+
+public:
+	AMyProjectCharacter();
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;
+
+	UFUNCTION(BlueprintCallable)
+	void PickUp(EItemClass Item);
+
+	UFUNCTION(BlueprintCallable)
+	void OnDamageTaken(float Damage);
+
+	UFUNCTION(BlueprintCallable)
+	void OnHealed(float Heal);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void KillPlayer();
+	void KillPlayer_Implementation();
+
+protected:
+
+	/** Resets HMD orientation in VR. */
+	void OnResetVR();
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/** 
+	 * Called via input to turn at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate. 
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	/** Handler for when a touch input begins. */
+	void TouchStarted(ETouchIndex::Type FingerIndex, FVector Location);
+
+	/** Handler for when a touch input stops. */
+	void TouchStopped(ETouchIndex::Type FingerIndex, FVector Location);
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// End of APawn interface
+
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
+
